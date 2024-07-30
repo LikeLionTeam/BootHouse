30ee6e3ecad1143beeecece8e9323e80386f6b12
minjikimkim2222 <olivia0929@naver.com>
2024-07-30 22:50:41 +0900

    Feat: bootcamp, category, course domain

---
+++ db/src/main/java/likelion/eight/category/CategoryJpaRepository.java
@@ -0,0 +1,7 @@
+package likelion.eight.category;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryJpaRepository extends JpaRepository<CategoryEntity, Long> {
+}
+
