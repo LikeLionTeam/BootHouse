0f2f7826be4e104c29a39f19d0f6a80a9f48d308
yeji <dldpwl1705@naver.com>
2024-08-02 21:41:57 +0900

    feat: 공지사항 crud 추가

--- view/src/main/java/likelion/eight/domain/notice/controller/NoticeController.java
+++ view/src/main/java/likelion/eight/domain/notice/controller/NoticeController.java
@@ -1,12 +1,15 @@
 package likelion.eight.domain.notice.controller;
 
-import likelion.eight.domain.notice.model.NoticeReq;
+import likelion.eight.domain.notice.model.request.NoticeReq;
+import likelion.eight.domain.notice.model.response.NoticeDetailRes;
+import likelion.eight.domain.notice.model.response.NoticeRes;
 import likelion.eight.domain.notice.service.NoticeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -29,6 +32,39 @@ public class NoticeController {
         } else {
             return ResponseEntity.ok(result);
         }
+    }
+
+    @PutMapping("/admin/notice/{noticeId}")
+    public ResponseEntity<?> updateNotice(@PathVariable Long noticeId,
+                                          @RequestBody NoticeReq noticeReq) {
+        // todo: 작성자의 게시글인지 확인
+
+        noticeService.updateNotice(noticeId, noticeReq);
+
+        return ResponseEntity.ok("update success");
+    }
+
+    @DeleteMapping("/admin/notice/{noticeId}")
+    public ResponseEntity<?> deleteNotice(@PathVariable Long noticeId) {
+        // todo: 작성자의 게시글인지 확인
+
+        noticeService.deleteNotice(noticeId);
+
+        return ResponseEntity.ok("delete success");
+    }
+
+    @GetMapping("/notice/{noticeId}")
+    public ResponseEntity<?> getNoticeDetail(@PathVariable Long noticeId) {
+        NoticeDetailRes noticeDetail = noticeService.getNoticeDetail(noticeId);
+
+        return ResponseEntity.ok(noticeDetail);
+    }
+
+    @GetMapping("/notice")
+    public ResponseEntity<?> getAllNotices() {
+        List<NoticeRes> allNotices = noticeService.getAllNotices();
+
+        return ResponseEntity.ok(allNotices);
     }
 
 }
--- view/src/main/java/likelion/eight/domain/notice/service/NoticeService.java
+++ view/src/main/java/likelion/eight/domain/notice/service/NoticeService.java
@@ -1,11 +1,15 @@
 package likelion.eight.domain.notice.service;
 
+import likelion.eight.common.domain.exception.ResourceNotFoundException;
 import likelion.eight.domain.notice.converter.MarkdownConverter;
-import likelion.eight.domain.notice.model.NoticeReq;
+import likelion.eight.domain.notice.model.request.NoticeReq;
+import likelion.eight.domain.notice.model.response.NoticeDetailRes;
+import likelion.eight.domain.notice.model.response.NoticeRes;
 import likelion.eight.notice.NoticeEntity;
 import likelion.eight.notice.NoticeJpaRepository;
 import likelion.eight.notice.enums.PostType;
 import lombok.RequiredArgsConstructor;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -15,9 +19,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
 
 
 @Service
@@ -58,6 +60,58 @@ public class NoticeService {
             result.put("error","파일 없음");
             return result;
         }
+    }
+
+    public void updateNotice(Long noticeId, NoticeReq noticeReq) {
+        NoticeEntity notice = getNotice(noticeId);
+
+        String content = markdownConverter.convertMarkdownToHtml(noticeReq.getContent());
+
+        notice.setTitle(noticeReq.getTitle());
+        notice.setContent(content);
+        notice.setPostType(PostType.valueOf(noticeReq.getPostType()));
+        notice.setImportance(noticeReq.getImportance());
+
+        noticeJpaRepository.save(notice);
+    }
+
+
+
+    public void deleteNotice(Long noticeId) {
+        noticeJpaRepository.deleteById(noticeId);
+    }
+
+    public NoticeDetailRes getNoticeDetail(Long noticeId) {
+        NoticeEntity notice = getNotice(noticeId);
+        String content = markdownConverter.convertHtmlToMarkdown(notice.getContent());
+        return NoticeDetailRes.builder()
+                .title(notice.getTitle())
+                .content(content)
+                .postType(notice.getPostType().toString())
+                .importance(notice.getImportance())
+                .build();
+    }
+
+    public List<NoticeRes> getAllNotices() {
+        List<NoticeEntity> allNotices = noticeJpaRepository.findAll();
+
+        return allNotices.stream().map(notice -> NoticeRes.builder()
+                        .title(notice.getTitle())
+                        .postType(notice.getPostType().toString())
+                        .importance(notice.getImportance())
+                        .build())
+                .toList();
+    }
+
+    @NotNull
+    private NoticeEntity getNotice(Long noticeId) {
+        Optional<NoticeEntity> noticeOptional = noticeJpaRepository.findById(noticeId);
+
+        if (noticeOptional.isEmpty()) {
+            throw new ResourceNotFoundException("notice", noticeId);
+        }
+
+        return noticeOptional.get();
     }
 
     private String storeFile(MultipartFile file){
