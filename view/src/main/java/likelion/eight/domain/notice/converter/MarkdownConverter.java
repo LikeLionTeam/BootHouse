6eaff3c06413bdd0dc90edf09ba260d0cde5ba4f
yeji <dldpwl1705@naver.com>
2024-08-02 21:41:14 +0900

    feat: 마크다운 변환 기능 추가
    마크업->마크다운으로 변경하는 메서드

--- view/src/main/java/likelion/eight/domain/notice/converter/MarkdownConverter.java
+++ view/src/main/java/likelion/eight/domain/notice/converter/MarkdownConverter.java
@@ -1,17 +1,23 @@
 package likelion.eight.domain.notice.converter;
 
 import com.vladsch.flexmark.html.HtmlRenderer;
+import com.vladsch.flexmark.html2md.converter.FlexmarkHtmlConverter;
 import com.vladsch.flexmark.parser.Parser;
 import com.vladsch.flexmark.util.data.MutableDataSet;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MarkdownConverter {
-    public String convertMarkdownToHtml(String markdown){
+    public String convertMarkdownToHtml(String markdown) {
         MutableDataSet options = new MutableDataSet();
         Parser parser = Parser.builder(options).build();
         HtmlRenderer htmlRenderer = HtmlRenderer.builder(options).build();
 
         return htmlRenderer.render(parser.parse(markdown));
+    }
+
+    public String convertHtmlToMarkdown(String html) {
+        MutableDataSet options = new MutableDataSet();
+        return FlexmarkHtmlConverter.builder(options).build().convert(html);
     }
 }
