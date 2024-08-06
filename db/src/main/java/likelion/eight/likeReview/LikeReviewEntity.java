90c332b3663d11e5425d923c9054657f73be11ab
yeji <dldpwl1705@naver.com>
2024-08-06 09:14:30 +0900

    feat: LikeReview 초안 작성
    controller,service,repository,entity 작성

--- db/src/main/java/likelion/eight/likeReview/LikeReviewEntity.java
+++ db/src/main/java/likelion/eight/likeReview/LikeReviewEntity.java
@@ -1,32 +1,32 @@
-package likelion.eight.userinterest;
+package likelion.eight.likeReview;
 
 import jakarta.persistence.*;
 import likelion.eight.BaseTimeEntity;
-import likelion.eight.course.CourseEntity;
+import likelion.eight.review.ReviewEntity;
 import likelion.eight.user.UserEntity;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
 
 @Entity
-@Table(name = "user_interests")
+@Table(name = "like_review")
 @Getter
 @Setter
 @EqualsAndHashCode(callSuper = true)
 @SuperBuilder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class UserInterestEntity extends BaseTimeEntity {
+public class LikeReviewEntity extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "user_interest_id")
+    @Column(name = "like_review_id")
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private UserEntity userEntity;
 
-
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "course_id")
-    private CourseEntity courseEntity;
+    @JoinColumn(name = "review_id")
+    private ReviewEntity reviewEntity;
+
 }
---
+++ db/src/main/java/likelion/eight/likeReview/LikeReviewJpaRepository.java
@@ -0,0 +1,6 @@
+package likelion.eight.likeReview;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LikeReviewJpaRepository extends JpaRepository<LikeReviewEntity,Long> {
+}
