93aef6bdea10293aab4fd639f20fcbe94f1532a0
yeji <dldpwl1705@naver.com>
2024-08-20 13:18:40 +0900

    feat: 네이버 지도 api
    로그인 하지 않은 사용자도 지도를 볼 수 있게 whiteList 수정
    Related: #71

--- view/src/main/java/likelion/eight/common/config/web/WebConfig.java
+++ view/src/main/java/likelion/eight/common/config/web/WebConfig.java
@@ -44,6 +44,7 @@ public class WebConfig implements WebMvcConfigurer {
     );
 
 
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(authorizationInterceptor)
